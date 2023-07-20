077d49ee147bb0352a20377f404297278071d527
chaehyeong <103627328+chaehyeong@users.noreply.github.com>
2023-07-20 17:00:20 +0900

    Update script.js

--- 2023/script.js
+++ 2023/script.js
@@ -7,7 +7,7 @@ nbsp.addEventListener('click', function(){
 
 function clickcount(){
     click++;
-    if (click >= 7){
+    if (click >= 10){
         alert('giga@hcu.hs.kr로 이 화면을 캡쳐해서 보내주시면 선착순 3명에게 선물을!')
         click = 0
     }
